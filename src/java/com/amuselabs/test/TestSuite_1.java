4e65897a5ec702501d0700165e84add6eb2067c4
ashutoshbadoni18 <ashutoshbadoni1997@gmail.com>
2018-05-24 09:08:12 +0530

    To,From,Cc,Bcc Test Cases

--- src/java/com/amuselabs/test/TestSuite_1.java
+++ src/java/com/amuselabs/test/TestSuite_1.java
@@ -1,66 +1,96 @@
 package com.amuselabs.test;
-//import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.Set;
-//import java.util.Properties;
-//import java.util.Set;
-
-//import org.junit.jupiter.api.AfterEach;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.util.Assert;
-
-import static java.lang.System.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class TestSuite_1 {
     String nametocheck;
     String value_in_From;
+    String extracted_strings_To[];
     WebDriver driver = new ChromeDriver();
+
     @BeforeAll
     public static void start_epadd() {
         try {
             StepDefs browser;
             browser = new StepDefs();
-            // browser.openBrowser("chrome");
             browser.openEpadd("appraisal");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     @BeforeEach
     public void open_Correspondents() {
             driver.get("http://localhost:9099/epadd/correspondents");
-            testOnClickName();
     }
 
     @Test
-    public void testOnClickName() {
-        WebElement e = driver.findElement(By.xpath("//*[@id=\"people\"]/tbody/tr[2]/td[1]/a"));
-        WebElement onClickValue = driver.findElement(By.xpath("//*[@id=\"people\"]/tbody/tr[2]/td[1]/a"));
-        nametocheck = onClickValue.getText();
-        e.click();
-        //testOnClickName(driver);
-        //String onClickValue = driver.findElement(By.cssSelector("#people > tbody > tr:nth-child(2) > td:nth-child(1)")).getText();
+    public void testFrom() {
+        nametocheck=new Helper().clickOnNameInCorrespondents(driver);
         value_in_From = new Helper().strings_From(driver);
-        String s = value_in_From;
-     //   assertFalse(value_in_From.contains(nametocheck));
+        assertTrue(value_in_From.contains(nametocheck));
     }
-    @AfterEach
-    public void check()
+
+     @Test
+     public void testTo()
      {
-     //    value_in_From = new Helper().strings_From(driver);
-         assertFalse(value_in_From.contains(nametocheck));
+         nametocheck=new Helper().clickOnNameInCorrespondents(driver);
+         extracted_strings_To=new Helper().strings_To(driver);
+         for(int i=0;i<extracted_strings_To.length;i++)
+         {
+             if(extracted_strings_To[i].contains(nametocheck))
+             {
+                 assertTrue(extracted_strings_To[i].contains(nametocheck));
+                 break;
+             }
+         }
      }
+
+     @Test
+     public void testCc() {
+         boolean result = false;
+         try {
+             nametocheck=new Helper().clickOnNameInCorrespondents(driver);
+             String[] extracted_strings_cc = new Helper().strings_Cc(driver);
+             for (int i = 0; i < extracted_strings_cc.length; i++) {
+                 if (extracted_strings_cc[i].contains(nametocheck)) {
+                     result = true;
+                     break;
+                 }
+             }
+         } catch (NoSuchElementException e) {
+             result = true;
+         }
+         assertTrue(result);
+     }
+
+     @Test
+      public void testBcc()
+     {
+         boolean result = false;
+         try {
+             nametocheck=new Helper().clickOnNameInCorrespondents(driver);
+             String[] extracted_strings_bcc = new Helper().strings_bcc(driver);
+             for (int i = 0; i < extracted_strings_bcc.length; i++) {
+                 if (extracted_strings_bcc[i].contains(nametocheck)) {
+                     result = true;
+                     break;
+                 }
+             }
+         } catch (NoSuchElementException e) {
+             result = true;
+         }
+         assertTrue(result);
+     }
+
+     @AfterEach
+    public void check() {
+        driver.close();
+        driver.quit();
+    }
+
 }
