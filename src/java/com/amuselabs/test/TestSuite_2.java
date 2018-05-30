9610aa9904d815843325767b86e6733423efe9e8
ashutoshbadoni18 <ashutoshbadoni1997@gmail.com>
2018-05-30 09:46:01 +0530

    Improved Test Cases for Person-Entities

--- src/java/com/amuselabs/test/TestSuite_2.java
+++ src/java/com/amuselabs/test/TestSuite_2.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -18,13 +18,7 @@ public class TestSuite_2
 
     @BeforeAll
     public static void start_epadd() {
-        try {
-            StepDefs browser;
-            browser = new StepDefs();
-            browser.openEpadd("appraisal");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Helper.start_ePADD();
     }
 
      @BeforeEach
@@ -33,13 +27,13 @@ public class TestSuite_2
          driver.get("http://localhost:9099/epadd/browse-top");
          WebElement e=driver.findElement(By.cssSelector("#all-cards > div:nth-child(2)"));
          e.click();
-         new Helper().waitFor();
+         Helper.waitFor();
      }
 
      @Test
     public void testNumber()
      {
-         int number=new Helper().message_number_PersonEntity(driver); //return the message number(1321 in this case)and also clicks ona name in page.
+         int number=Helper.message_number_PersonEntity(driver); //return the message number(1321 in this case)and also clicks ona name in page.
          WebElement actual_number=driver.findElement(By.cssSelector("#pageNumbering"));//actual message number(y) of form x/y
          String actualNumber=actual_number.getText();
          actualNumber=actualNumber.substring(2);
@@ -49,9 +43,9 @@ public class TestSuite_2
      @Test
      public void testName()
      {
-         String name=new Helper().name_from_Person_Entity(driver);
-         new Helper().waitFor();
-         new Helper().changeWindow(driver);
+         String name=Helper.name_from_Person_Entity(driver);
+         Helper.waitFor();
+         Helper.changeWindow(driver);
          WebElement e1=driver.findElement(By.cssSelector("#jog_contents > div.muse-doc > div.muse-doc-body"));
          String body=e1.getText();
          assertTrue(body.contains(name));//checks for the name only in body of mail not in Cc,Bcc,To and From.
@@ -60,13 +54,13 @@ public class TestSuite_2
      public void testPerson_Entity_Name_Advanced_Search()
      {
         String name=new Helper().name_from_Person_Entity(driver);
-        new Helper().changeWindow(driver);
+        Helper.changeWindow(driver);
         WebElement e1=driver.findElement(By.cssSelector("#bs-example-navbar-collapse > ul:nth-child(1) > li:nth-child(3) > a"));
         e1.click();
-        new Helper().go_to_advanced_Search(driver);
+        Helper.go_to_advanced_Search(driver);
         WebElement e2=driver.findElement(By.cssSelector("#entity"));
         e2.sendKeys(name);
-        new Helper().clickOnSearch_InAdvancedSearchPage(driver);
+        Helper.clickOnSearch_InAdvancedSearchPage(driver);
         WebElement e3=driver.findElement(By.cssSelector("#jog_contents"));
         String whole_text=e3.getText();
         assertTrue(whole_text.contains(name));
@@ -79,13 +73,13 @@ public class TestSuite_2
          WebElement e1=driver.findElement(By.cssSelector("#entities > tbody > tr:nth-child(1) > td.sorting_2"));
          int message_number=Integer.parseInt(e1.getText());
          e1.click();
-         new Helper().changeWindow(driver);
+         Helper.changeWindow(driver);
          WebElement e2=driver.findElement(By.cssSelector("#bs-example-navbar-collapse > ul:nth-child(1) > li:nth-child(3) > a"));
          e2.click();
          new Helper().go_to_advanced_Search(driver);
          WebElement e3=driver.findElement(By.cssSelector("#entity"));
          e3.sendKeys(name);
-         new Helper().clickOnSearch_InAdvancedSearchPage(driver);
+         Helper.clickOnSearch_InAdvancedSearchPage(driver);
          WebElement e4=driver.findElement(By.cssSelector("#pageNumbering"));
          String num=e4.getText().substring(2);
          int new_number=Integer.parseInt(num);
@@ -94,27 +88,57 @@ public class TestSuite_2
      @Test
      public void testNumberBrowseTopPagePersonEntities()
      {
-       int number=new Helper().number_in_PersonEntity_BrowseTopPage(driver);
-       new Helper().clickOnEditEntities_PersonEntity(driver);
-       int number_Of_Contacts=new Helper().countNumberOfContactsIn_a_Page(driver);
+       int number=Helper.number_in_PersonEntity_BrowseTopPage(driver);
+       Helper.clickOnEditEntities_PersonEntity(driver);
+       int number_Of_Contacts=Helper.countNumberOfContactsIn_a_Page(driver);
        assertFalse(number==number_Of_Contacts);
      }
      @Test
      public void test_Sent_and_Received()
      {
-         new Helper().name_from_Person_Entity(driver);
-         new Helper().waitFor();
-         new Helper().changeWindow(driver);
-         int number_of_sentmessages=new Helper().sent_messages_in_PersonEntitiesPage(driver);
-         int number_of_receivedmessages=new Helper().received_messages_in_PersonEntitiesPage(driver);
+         Helper.name_from_Person_Entity(driver);
+         Helper.waitFor();
+         Helper.changeWindow(driver);
+         int number_of_sentmessages=Helper.sent_messages_in_PersonEntitiesPage(driver);
+         int number_of_receivedmessages=Helper.received_messages_in_PersonEntitiesPage(driver);
          WebElement e=driver.findElement(By.cssSelector("#pageNumbering"));
          int number_of_messages_opened=Integer.parseInt(e.getText().substring(2));
          int sum=number_of_sentmessages+number_of_receivedmessages;
          assertEquals(sum,number_of_messages_opened);
      }
+     @Test
+     public void testPerson_Entity_Name_Underlined_or_Not() {
+         String name = Helper.name_from_Person_Entity(driver);
+         Helper.changeWindow(driver);
+         List<WebElement> underline_entities =Helper.strings_underlined(driver);
+         int count = 0;
+         for (WebElement e : underline_entities) {
+             String s = e.getText();
+             if (s.compareTo(name) == 0) {
+                 count++;
+             }
+             assertTrue(count == 6);
+         }
+     }
+     @Test
+     public void testPerson_Entity_Name_highlighted_or_Not()
+     {
+         String name=Helper.name_from_Person_Entity(driver);
+         Helper.changeWindow(driver);
+         List<WebElement> highlighted_entities=Helper.strings_highlighted(driver);
+         int count=0;
+         for(WebElement e:highlighted_entities)
+         {
+             String s=e.getText();
+             if(s.compareTo(name)==0) {
+                 count++;
+             }
+         }
+         assertTrue(count==6);
+     }
      @AfterEach
      public void post_Set()
      {
-         driver.quit();
+         //driver.quit();
      }
 }
