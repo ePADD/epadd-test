5ffec0652f64aa61097eaba213368a51234f63c4
ashutoshbadoni18 <ashutoshbadoni1997@gmail.com>
2018-05-30 09:44:55 +0530

    Improved Test Cases for Correspondents

--- src/java/com/amuselabs/test/TestSuite_1.java
+++ src/java/com/amuselabs/test/TestSuite_1.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -22,13 +20,7 @@ public class TestSuite_1 {
 
     @BeforeAll
     public static void start_epadd() {
-        try {
-            StepDefs browser;
-            browser = new StepDefs();
-            browser.openEpadd("appraisal");  //BeforeAll contains code for starting ePADD
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+       Helper.start_ePADD();
     }
 
     @BeforeEach
@@ -36,21 +28,21 @@ public class TestSuite_1 {
             driver.get("http://localhost:9099/epadd/browse-top");
             WebElement e=driver.findElement(By.cssSelector("#all-cards > div:nth-child(1)")); //BeforeEach contains code that navigates to Correspondents
             e.click();                                                                        //Page
-            new Helper().waitFor();
+            Helper.waitFor();
     }
 
     @Test
     public void testFrom() {
-        nametocheck=new Helper().clickOnNameInCorrespondents(driver);//finds a name ,store its text,click on it and returns name in "nametocheck" variable
-        value_in_From = new Helper().strings_From(driver);//returns string contained in "From:" of mail.
-        assertTrue(value_in_From.contains(nametocheck));//This TestCase fails if "From" is not present or if name which was clicked was not present in "From"
+        nametocheck=Helper.clickOnNameInCorrespondents(driver);//finds a name ,store its text,click on it and returns name in "nametocheck" variable
+        value_in_From =Helper.strings_From(driver);//returns string contained in "From:" of mail.
+        assertTrue(value_in_From.contains(nametocheck)); //This TestCase fails if "From" is not present or if name which was clicked was not present in "From"
     }
 
      @Test
      public void testTo()
      {
-         nametocheck=new Helper().clickOnNameInCorrespondents(driver);
-         extracted_strings_To=new Helper().strings_To(driver);
+         nametocheck=Helper.clickOnNameInCorrespondents(driver);
+         extracted_strings_To=Helper.strings_To(driver);
          for(int i=0;i<extracted_strings_To.length;i++)
          {
              if(extracted_strings_To[i].contains(nametocheck))  //same test case as above,checks string in "To" of mail.
@@ -65,8 +57,8 @@ public class TestSuite_1 {
      public void testCc() {
          boolean result = false;
          try {
-             nametocheck=new Helper().clickOnNameInCorrespondents(driver);
-             String[] extracted_strings_cc = new Helper().strings_Cc(driver); //same test case as above two,checks in "Cc" of mail.
+             nametocheck=Helper.clickOnNameInCorrespondents(driver);
+             String[] extracted_strings_cc =Helper.strings_Cc(driver); //same test case as above two,checks in "Cc" of mail.
              for (int i = 0; i < extracted_strings_cc.length; i++) {
                  if (extracted_strings_cc[i].contains(nametocheck)) {  //this test case fails only if "Cc" is present and name clicked is not present in it.
                      result = true;
@@ -84,8 +76,8 @@ public class TestSuite_1 {
      {
          boolean result = false;
          try {
-             nametocheck=new Helper().clickOnNameInCorrespondents(driver);
-             String[] extracted_strings_bcc = new Helper().strings_bcc(driver); //same test case as above two,checks in "bcc" of mail.
+             nametocheck=Helper.clickOnNameInCorrespondents(driver);
+             String[] extracted_strings_bcc = Helper.strings_bcc(driver); //same test case as above two,checks in "bcc" of mail.
              for (int i = 0; i < extracted_strings_bcc.length; i++) {
                  if (extracted_strings_bcc[i].contains(nametocheck)) {    //this test case fails only if "bcc" is present and name clicked is not present in it.
                      result = true;
@@ -103,8 +95,8 @@ public class TestSuite_1 {
       {
           WebElement e = driver.findElement(By.xpath("//*[@id=\"people\"]/tbody/tr[2]/td[1]/a"));
           e.click();                  //finds a Correspondent's name and clicks on it
-          new Helper().waitFor();
-          id=new Helper().string_ID(driver);
+          Helper.waitFor();
+          id=Helper.string_ID(driver);
           id=id.substring(0,id.length()-6);   //getting the id of message
           driver.get("http://localhost:9099/epadd/advanced-search");
           WebElement e1=driver.findElement(By.cssSelector("#uniqueId"));
@@ -121,27 +113,34 @@ public class TestSuite_1 {
       public void testNumberBrowseTopPageCorrespondents()
       {
           int number=new Helper().number_in_Correspondents_BrowseTopPage(driver);
-          new Helper().clickOnEditCorrespondents_Correspondents(driver);
+          Helper.clickOnEditCorrespondents_Correspondents(driver);
           int number_Of_Contacts=new Helper().countNumberOfContactsIn_a_Page(driver);
           assertTrue(number==number_Of_Contacts);
       }
       @Test
       public void test_Sent_and_Received()
       {
-          new Helper().clickOnNameInCorrespondents(driver);
-          new Helper().waitFor();
-          new Helper().changeWindow(driver);
-          int number_of_sentmessages=new Helper().sent_messages_in_CorrespondentsPage(driver);
-          int number_of_receivedmessages=new Helper().received_messages_in_CorrespondentsPage(driver);
+          Helper.clickOnNameInCorrespondents(driver);
+          Helper.waitFor();
+          Helper.changeWindow(driver);
+          int number_of_sentmessages=Helper.sent_messages_in_CorrespondentsPage(driver);
+          int number_of_receivedmessages=Helper.received_messages_in_CorrespondentsPage(driver);
           WebElement e=driver.findElement(By.cssSelector("#pageNumbering"));
           int number_of_messages_opened=Integer.parseInt(e.getText().substring(2));
           int sum=number_of_sentmessages+number_of_receivedmessages;
           assertEquals(sum,number_of_messages_opened);
       }
+     /* @Test
+      public void testCorrespondentName_Underlined_or_Not()
+      {
+         String name=Helper.clickOnNameInCorrespondents(driver);
+          Helper.changeWindow(driver);
+          Helper.strings_underlined(driver);
+      }*/
      @AfterEach
     public void check()
      {
-       // driver.quit();
+        driver.quit();
      }
 
 }
