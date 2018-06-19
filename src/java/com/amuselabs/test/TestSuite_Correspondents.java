aedde0f11b7f2b41825b06dc58977f2fa5d2633f
ashutoshbadoni18 <ashutoshbadoni1997@gmail.com>
2018-06-19 15:30:48 +0530

    improved test cases including basic sanity check for Person-Entities

--- src/java/com/amuselabs/test/TestSuite_Correspondents.java
+++ src/java/com/amuselabs/test/TestSuite_Correspondents.java
@@ -2,17 +2,14 @@ package com.amuselabs.test;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.opentest4j.AssertionFailedError;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.List;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -23,7 +20,7 @@ public class TestSuite_Correspondents
     String nametocheck;   //Variable for name in "All Correspondents" Page,eg "Kathleen Shanahan","Eli Ferrera",etc.
     WebDriver driver = new ChromeDriver();
       static int flag=0;
-    public static Properties correspondents = new Properties();   //correspondents variable representing, reading selectors from properties file for Correspondents.
+    public static Properties user_interface = new Properties();   //user_interface variable representing, reading selectors from properties file for Correspondents.
 
     @BeforeAll
     public static void start_epadd() {
@@ -31,7 +28,7 @@ public class TestSuite_Correspondents
             Helper.start_ePADD();    //method for starting ePADD
 
             InputStream s = new FileInputStream("/home/ashu18/Projects/epadd_dev/epadd-test/src/java/com/amuselabs/test/USER_INTERFACE.properties");
-            correspondents.load(s);       //Reading properties files
+            user_interface.load(s);       //Reading properties files
         }
         catch(Exception e)
         {
@@ -47,14 +44,14 @@ public class TestSuite_Correspondents
     }
 
     @Test
-    public void testFrom()
+    public void testFrom()   //This TestCase fails if "From" is not present or if name which was clicked was not present in "From"
     {
 
         try {
             int contact_id = Helper.get_contact_id(driver);
             nametocheck = Helper.clickOnNameInCorrespondents(driver);  //finds a name ,store its text,click on it and returns name in "nametocheck" variable
             int contact_id_From = Helper.get_contact_id_From(driver);
-            assertEquals(contact_id, contact_id_From);    //This TestCase fails if "From" is not present or if name which was clicked was not present in "From"
+            assertEquals(contact_id, contact_id_From);
         }
         catch(AssertionFailedError e)
         {
@@ -63,11 +60,11 @@ public class TestSuite_Correspondents
     }
 
      @Test
-     public void testTo()
+     public void testTo()    //same test case as above,checks string in "To" of mail
      {
          try {
              int contact_id = Helper.get_contact_id(driver);
-             nametocheck = Helper.clickOnNameInCorrespondents(driver);   //same test case as above,checks string in "To" of mail
+             nametocheck = Helper.clickOnNameInCorrespondents(driver);
              int contact_id_to = Helper.get_contact_id_To(driver);
              assertEquals(contact_id, contact_id_to);
          }
@@ -136,7 +133,7 @@ public class TestSuite_Correspondents
           Helper.clickOnNameInCorrespondents(driver);   //finds a Correspondent's name and clicks on it
           Helper.waitFor();
           String id=Helper.string_ID(driver);           //getting the id of message
-          Helper.click_on_search_through_correspondents(driver);
+          Helper.click_on_search(driver);
           Helper.waitFor();
           Helper.go_to_advanced_Search(driver);
           Helper.waitFor();
@@ -146,9 +143,9 @@ public class TestSuite_Correspondents
           assertTrue(id.equals(newID));
       }
       @Test
-      public void testNumberBrowseTopPageCorrespondents() //this test case matches the total correspondents with the number of contacts in correspondent's address book.
+      public void testNumberBrowseTopPageCorrespondents() //this test case matches the total user_interface with the number of contacts in correspondent's address book.
       {
-          int number=Helper.number_in_Correspondents_BrowseTopPage(driver);    //returns total correspondents number,(eg it returns (2251) since we have Correspondents(2251) in our browse-top page.
+          int number=Helper.number_in_Correspondents_BrowseTopPage(driver);    //returns total user_interface number,(eg it returns (2251) since we have Correspondents(2251) in our browse-top page.
           Helper.clickOnEditCorrespondents_Correspondents(driver);
           int number_Of_Contacts=Helper.countNumberOfContactsIn_a_Page(driver);   //counts and return number of contacts in address book.
           assertTrue(number==number_Of_Contacts);
--- src/java/com/amuselabs/test/TestSuite_Person_Entities.java
+++ src/java/com/amuselabs/test/TestSuite_Person_Entities.java
@@ -21,7 +21,7 @@ public class TestSuite_Person_Entities
 {
     WebDriver driver=new ChromeDriver();
     String id;
-    public static Properties person_entities =new Properties(); //correspondents variable corresponding to the Properties file for Person-Entities.
+    public static Properties user_interface =new Properties(); //user_interface variable corresponding to the Properties file for Person-Entities.
 
 
     @BeforeAll
@@ -29,7 +29,7 @@ public class TestSuite_Person_Entities
         try {
             Helper.start_ePADD();
             InputStream s = new FileInputStream("/home/ashu18/Projects/epadd_dev/epadd-test/src/java/com/amuselabs/test/USER_INTERFACE.properties");
-            person_entities.load(s);  //Reading properties file
+            user_interface.load(s);  //Reading properties file
         }
         catch(Exception e)
         {
@@ -40,133 +40,244 @@ public class TestSuite_Person_Entities
      @BeforeEach
     public void open_Person_entities()
      {
-
+     try {
          Helper.click_on_Person_Entities_BrowseTopPage(driver);
+         assertTrue(Helper.isPerson_Entities_Page_Opened(driver));
+     }
+     catch (AssertionFailedError e)
+     {
+         System.out.println("Incorrect page opened on clicking Person-Entities button on browse-top page");
+     }
      }
 
      @Test
-    public void testNumber()
-     {
+    public void testNumber() //this test case checks whether number displayed in front of Person-Entity(eg 1681 in front of Jeb Bush)
+     {                       //is equal to the number of messages opened on clicking that particular Person-Entity or not.
          int number=0;
          int actualNumber=0;
+         int check=0;
          try {
-              number = Helper.message_number_PersonEntity(driver); //return the message number(1321 in this case)and also clicks on a name in page.
+              number = Helper.get_message_number_PersonEntity_and_click_on_name(driver); //return the message number(1321 in this case)and also clicks on a name in page.
+             assertTrue(Helper.isMessage_Window_Page_Opened(driver));
+             check=1;
              actualNumber = Helper.number_of_messages_opened_after_clicking_on_PersonEntities_name(driver);
              assertEquals(number, actualNumber);
          }
         catch (AssertionFailedError e)
          {
-           //  driver.get("http://localhost:9099/epadd/list-entities.js");
-             System.out.println("Number of messages mentioned in front of name"+"::"+/*Helper.name_from_Person_Entity(driver)*/" "+"is"+"::"+number);
-             System.out.println("Actual number of messages opened on clicking name"+"::"+/*Helper.name_from_Person_Entity(driver)+*/" "+"is"+"::"+actualNumber);
+            if(check==0)
+            {
+                System.out.println("Incorrect page opened on clicking Person-Entities name");
+            }
+            if(check!=0) {
+                System.out.println("Number of messages mentioned in front of name" + "=" + number);
+                System.out.println("Actual number of messages opened on clicking name" + "=" + actualNumber);
+            }
          }
      }
      @Test
-     public void testName()
+     public void testName() //this test case clicks on a person-entity ,stores it,and checks whether the mail opened contains that person entity or not.
      {
-         String name=Helper.name_from_Person_Entity(driver);
-         Helper.waitFor();
-         Helper.changeWindow(driver);
-         String body=Helper.body_of_mail_after_clicking_a_name_in_PersonEntity(driver);
-         assertTrue(body.contains(name));//checks for the name only in body of mail not in Cc,Bcc,To and From.
+         int check=0;
+         try {
+             String name = Helper.get_name_from_Person_Entity_and_click_on_name(driver);
+             assertTrue(Helper.isMessage_Window_Page_Opened(driver));
+             check=1;
+             String body = Helper.body_of_mail_after_clicking_a_name_in_PersonEntity(driver);
+             assertTrue(body.contains(name));//checks for the name only in body of mail not in Cc,Bcc,To and From.
+         }
+         catch (AssertionFailedError e)
+         {
+             if(check==0)
+             {
+                 System.out.println("Incorrect page opened on clicking Person-Entities name");
+             }
+             if(check!=0)
+             {
+                 System.out.println("Name clicked is not present in the body of mail opened::");
+             }
+         }
      }
      @Test
-     public void testPerson_Entity_Name_Advanced_Search()
-     {
-        String name=new Helper().name_from_Person_Entity(driver);
-        Helper.changeWindow(driver);
-        Helper.click_on_search_through_PersonEntities(driver);
-        Helper.go_to_advanced_Search(driver);
-        Helper.enter_data_in_Entity_Textfield_InAdvancedSearchPage(driver,name);
-        Helper.clickOnSearch_InAdvancedSearchPage(driver);
-        String whole_text=Helper.whole_mail_after_entering_entity_in_Advanced_Search_Page(driver);
-        assertTrue(whole_text.contains(name));
+     public void testPerson_Entity_Name_Advanced_Search() //this test case clicks on a person-entity and stores it.
+     {                                                    //it then navigates to advanced search page,enters same person-entity in entity textfield
+         int check=0;                                   //and then clicks on search,it then checks whether the mail opened conatins that person-entity or not.
+         try {
+             String name = Helper.get_name_from_Person_Entity_and_click_on_name(driver);
+             assertTrue(Helper.isMessage_Window_Page_Opened(driver));
+             Helper.click_on_search(driver);
+             assertTrue(Helper.isSearch_Page_Opened(driver));
+             Helper.go_to_advanced_Search(driver);
+             assertTrue(Helper.isAdvanced_Search_Page_Opened(driver));
+             Helper.enter_data_in_Entity_Textfield_InAdvancedSearchPage(driver, name);
+             Helper.clickOnSearch_InAdvancedSearchPage(driver);
+             assertTrue(Helper.isMessage_window_page_opened_after_clicking_on_search_in_advanced_search_page(driver));
+             check=1;
+             String whole_text = Helper.whole_mail_after_entering_entity_in_Advanced_Search_Page(driver);
+             assertTrue(whole_text.contains(name));
+         }
+         catch (AssertionFailedError e)
+         {
+             if(check==0)
+             {
+                 System.out.println("Somewhere there is incorrect page opened on clicking a certain element");
+             }
+             if(check!=0)
+             {
+                 System.out.println("The name entered in \"Entity\" textfield is not present in mail opened after clicking on search");
+             }
+         }
      }
      @Test
-     public void testPerson_Entity_Message_Number_Advanced_Search()
-     {   int number=0,new_number=0;
+     public void testPerson_Entity_Message_Number_Advanced_Search() //this test case clicks on a person-entity and stores number menioned in front of it.
+     {                                                               //it then navigates to advanced search page,enters same person-entity in entity textfield
+         int number=0,new_number=0,check=0;                   //it then clicks on "Search" button,and checks whether the total messages opened are equal to number initially mentioned.
       try {
-          String message_number = TestSuite_Person_Entities.person_entities.getProperty("message_number");
+          String message_number = TestSuite_Person_Entities.user_interface.getProperty("message_number");
           WebElement message_number_of_a_PersonEntity = driver.findElement(By.cssSelector(message_number));//message number in front of name
           number = Integer.parseInt(message_number_of_a_PersonEntity.getText());
-          String name = Helper.name_from_Person_Entity(driver);
-          Helper.changeWindow(driver);
-          Helper.click_on_search_through_PersonEntities(driver);
-          new Helper().go_to_advanced_Search_through_PersonEntities(driver);
+          String name = Helper.get_name_from_Person_Entity_and_click_on_name(driver);
+          assertTrue(Helper.isMessage_Window_Page_Opened(driver));
+          Helper.click_on_search(driver);
+          assertTrue(Helper.isSearch_Page_Opened(driver));
+          Helper.go_to_advanced_Search(driver);
+          assertTrue(Helper.isAdvanced_Search_Page_Opened(driver));
           Helper.enter_data_in_Entity_Textfield_InAdvancedSearchPage(driver, name);
           Helper.clickOnSearch_InAdvancedSearchPage(driver);
+          assertTrue(Helper.isMessage_window_page_opened_after_clicking_on_search_in_advanced_search_page(driver));
+          check=1;
           new_number = Helper.number_of_messages_opened_after_clicking_on_PersonEntities_name(driver);
           assertEquals(number, new_number);
       }
       catch (AssertionFailedError e)
       {
-          System.out.println("There is a mismatch in the message number displayed in front of Person-entity and the number of messages opened through advanced search of same entity::");
-          System.out.println("Expected::"+number);
-          System.out.println("Found::"+new_number);
+          if(check==0)
+          {
+                  System.out.println("Somewhere there is incorrect page opened on clicking a certain element");
+
+          }
+          if(check!=0) {
+              System.out.println("There is a mismatch in the message number displayed in front of Person-entity and the number of messages opened through advanced search of same entity::");
+              System.out.println("Expected::" + number);
+              System.out.println("Found::" + new_number);
+          }
       }
      }
      @Test
-     public void testNumberBrowseTopPagePersonEntities()
-     {
-         int number=0,number_Of_Contacts=0;
+     public void testNumberBrowseTopPagePersonEntities() //this test case checks whether the number displayed in Person-Entities button in browse-top page
+     {                                                   //for eg(Person-Entity 2162) is equal to the number of contacts on that page or not.
+         int number=0,number_Of_Contacts=0,check=0;
          try {
-             number = Helper.number_in_PersonEntity_BrowseTopPage(driver);
+             number = Helper.get_number_in_PersonEntity_BrowseTopPage_and_click_on_Person_Entities(driver);
+             assertTrue(Helper.isPerson_Entities_Page_Opened(driver));
              Helper.clickOnEditEntities_PersonEntity(driver);
+             assertTrue(Helper.isEdit_entities_page_opened_Person_Entities(driver));
+             check=1;
              number_Of_Contacts = Helper.countNumberOfContactsIn_a_Page(driver);
              assertEquals(number, number_Of_Contacts);
          }
          catch (AssertionFailedError e)
          {
-             System.out.println("Number displayed in Person-entities button in browse-top page="+"::"+number);
-             System.out.println("Number of contacts present in Person-entities="+"::"+number_Of_Contacts);
-         }
-     }
-     @Test
-     public void test_Sent_and_Received()
-     {
-         Helper.name_from_Person_Entity(driver);
-         Helper.waitFor();
-         Helper.changeWindow(driver);
-         int number_of_sentmessages=Helper.sent_messages_in_PersonEntitiesPage(driver);
-         int number_of_receivedmessages=Helper.received_messages_in_PersonEntitiesPage(driver);
-         int number_of_messages_opened=Helper.number_of_messages_opened_after_clicking_on_PersonEntities_name(driver);
-         int sum=number_of_sentmessages+number_of_receivedmessages;
-         assertTrue(sum>=number_of_messages_opened);
-     }
-     @Test
-     public void testPerson_Entity_Name_Underlined_or_Not() {
-         String name = Helper.name_from_Person_Entity(driver);
-         Helper.changeWindow(driver);
-         List<WebElement> underline_entities =Helper.strings_underlined_on_message_window_of_Person_Entities(driver);
-         int count = 0;
-         for (WebElement e : underline_entities) {
-             String s = e.getText();
-             if (s.compareTo(name) == 0) {
-                 count++;
+             if(check==0)
+             {
+                 System.out.println("Somewhere there is incorrect page opened on clicking a certain element");
+             }
+             if(check!=0) {
+                 System.out.println("Number displayed in Person-entities button in browse-top page=" + number);
+                 System.out.println("Number of contacts present in Person-entities=" + number_Of_Contacts);
              }
          }
-         assertTrue(count == Integer.parseInt(TestSuite_Person_Entities.person_entities.getProperty("number_of_underlined_entity_you_see_in_page")));
      }
      @Test
-     public void testPerson_Entity_Name_highlighted_or_Not()
-     {
-         String name=Helper.name_from_Person_Entity(driver);
-         Helper.changeWindow(driver);
-         List<WebElement> highlighted_entities=Helper.strings_highlighted_on_message_window_of_Person_Entities(driver);
-         int count=0;
-         for(WebElement e:highlighted_entities)
+     public void test_Sent_and_Received()    //this test case checks whether the sum of sent and received messages present in message window of a particular
+     {                                       //Person-entity is greater than or equal to the number of messages opened or not.
+         int check=0;
+         try {
+             Helper.get_name_from_Person_Entity_and_click_on_name(driver);
+             assertTrue(Helper.isMessage_Window_Page_Opened(driver));
+             check=1;
+             int number_of_sent_messages = Helper.sent_messages_in_PersonEntitiesPage(driver);
+             int number_of_received_messages = Helper.received_messages_in_PersonEntitiesPage(driver);
+             int number_of_messages_opened = Helper.number_of_messages_opened_after_clicking_on_PersonEntities_name(driver);
+             int sum = number_of_sent_messages + number_of_received_messages;
+             assertTrue(sum >= number_of_messages_opened);
+         }
+         catch (AssertionFailedError e)
          {
-             String s=e.getText();
-             if(s.compareTo(name)==0) {
-                 count++;
+             if(check==0)
+             {
+                 System.out.println("Incorrect Page Opened on clicking a Person-Entities name");
+             }
+             if(check!=0)
+             {
+                 System.out.println("Assert Condition mismatch");
              }
          }
-         assertTrue(count == Integer.parseInt(TestSuite_Person_Entities.person_entities.getProperty("number_of_highlighted_entity_you_see_in_page")));
+     }
+     @Test
+     public void testPerson_Entity_Name_Underlined_or_Not()   //this test case checks whether the Person-Entitie's name clicked is underlined in the messages
+     {                                                        //opened or not.
+         int check=0;
+         try {
+             String name = Helper.get_name_from_Person_Entity_and_click_on_name(driver);
+             assertTrue(Helper.isMessage_Window_Page_Opened(driver));
+             check=1;
+             List<WebElement> underline_entities = Helper.strings_underlined_on_message_window_of_Person_Entities(driver);
+             int count = 0;
+             for (WebElement e : underline_entities) {
+                 String s = e.getText();
+                 if (s.compareTo(name) == 0) {
+                     count++;
+                 }
+             }
+             assertTrue(count == Integer.parseInt(TestSuite_Person_Entities.user_interface.getProperty("number_of_underlined_entity_you_see_in_page")));
+         }
+         catch (AssertionFailedError e)
+         {
+             if(check==0)
+             {
+                 System.out.println("Incorrect page opened on clicking Person-Entities name::");
+             }
+             if(check!=0)
+             {
+                 System.out.println("Name clicked is not underlined");
+             }
+         }
+     }
+     @Test
+     public void testPerson_Entity_Name_highlighted_or_Not()  //this test case checks whether the Person-Entities name clicked is highlighted in the
+     {                                                        //messages opened or not.
+         int check=0;
+         try {
+             String name = Helper.get_name_from_Person_Entity_and_click_on_name(driver);
+             assertTrue(Helper.isMessage_Window_Page_Opened(driver));
+             check=1;
+             List<WebElement> highlighted_entities = Helper.strings_highlighted_on_message_window_of_Person_Entities(driver);
+             int count = 0;
+             for (WebElement e : highlighted_entities) {
+                 String s = e.getText();
+                 if (s.compareTo(name) == 0) {
+                     count++;
+                 }
+             }
+             assertTrue(count == Integer.parseInt(TestSuite_Person_Entities.user_interface.getProperty("number_of_highlighted_entity_you_see_in_page")));
+         }
+              catch (AssertionFailedError e)
+             {
+                 if(check==0)
+                 {
+                     System.out.println("Incorrect page opened on clicking Person-Entities name::");
+                 }
+                 if(check!=0)
+                 {
+                     System.out.println("Name clicked is not highlighted");
+                 }
+             }
      }
      @AfterEach
      public void post_Set()
      {
 
-         driver.quit();
+      //  driver.quit();
      }
 }
