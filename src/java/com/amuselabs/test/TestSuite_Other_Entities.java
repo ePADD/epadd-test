ae802cfb7450c800a4fa03a8de1922bf150439e6
ashutoshbadoni18 <ashutoshbadoni1997@gmail.com>
2018-06-20 10:58:18 +0530

    modified Test_Suite for Other-Entities

--- src/java/com/amuselabs/test/TestSuite_Other_Entities.java
+++ src/java/com/amuselabs/test/TestSuite_Other_Entities.java
@@ -1,8 +1,6 @@
 package com.amuselabs.test;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.opentest4j.AssertionFailedError;
 
@@ -11,7 +9,6 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //Test cases for the "Other Entities" page.
@@ -19,14 +16,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TestSuite_Other_Entities
 {
     WebDriver driver=new ChromeDriver();
-    public static Properties other_entities =new Properties();
+    public static Properties user_interface =new Properties();
 
     @BeforeAll
     public static void start_epadd() {
         try {
             Helper.start_ePADD();
             InputStream s = new FileInputStream("/home/ashu18/Projects/epadd_dev/epadd-test/src/java/com/amuselabs/test/USER_INTERFACE.properties");
-            other_entities.load(s);   //Reading properties file
+            user_interface.load(s);   //Reading properties file
         }
         catch (Exception e)
         {
@@ -41,15 +38,14 @@ public class TestSuite_Other_Entities
     }
 
     @Test
-    public void testNumber()
-    {
+    public void testNumber()  //this test case checks whether the number displayed in front of subentity(eg 69 in front of United States) is equal to the number of
+    {                           //messages opened on clicking that particular subentity or not.
         int message_number=0,actual_number=0;
         try {
             Helper.clickOnEntityName_OtherEntitiesPage(driver);
             message_number = Helper.message_number_OtherEntitiesPage(driver);
             Helper.clickOnSubEntityName_OtherEntitiesPage(driver);
-            driver.get("http://localhost:9099/epadd/browse?archiveID=84e8afd01303201b1bb2e763c78c2df0f7133d3004e1ce6e04d9b5e2e3d15423&adv-search=1&term=%22United%20States%22&termBody=on&termSubject=on");
-            Helper.waitFor();
+           // driver.get("http://localhost:9099/epadd/browse?archiveID=84e8afd01303201b1bb2e763c78c2df0f7133d3004e1ce6e04d9b5e2e3d15423&adv-search=1&term=%22United%20States%22&termBody=on&termSubject=on");
             actual_number = Helper.number_of_messages_opened_after_clicking_on_subEntity_name(driver);
             assertEquals(message_number, actual_number);
         }
@@ -61,21 +57,21 @@ public class TestSuite_Other_Entities
         }
     }
     @Test
-    public void testEntityName()
+    public void testEntityName()  //this test case clicks on a sub-entity (eg United States)and checks whether the opened messages contains United States or not.
     {
         Helper.clickOnEntityName_OtherEntitiesPage(driver);
         String sub_entity_name=Helper.clickOnSubEntityName_OtherEntitiesPage(driver);
-        driver.get("http://localhost:9099/epadd/browse?archiveID=84e8afd01303201b1bb2e763c78c2df0f7133d3004e1ce6e04d9b5e2e3d15423&adv-search=1&term=%22United%20States%22&termBody=on&termSubject=on");
+      //  driver.get("http://localhost:9099/epadd/browse?archiveID=84e8afd01303201b1bb2e763c78c2df0f7133d3004e1ce6e04d9b5e2e3d15423&adv-search=1&term=%22United%20States%22&termBody=on&termSubject=on");
         String body=Helper.body_of_mail_opened_onclick_subEntity(driver);
         assertTrue(body.contains(sub_entity_name));
     }
      @Test
-     public void testEntityNameAdvacedSearch()
-     {
-         Helper.clickOnEntityName_OtherEntitiesPage(driver);
+     public void testEntityNameAdvacedSearch()  //this test case clicks on a sub-entity type,stores it,(eg United States),navigates till advanced search page
+     {                                          //enters that sub-entity type in "Entity" textfield,clciks on search, and then checks whether the messages opened contains
+         Helper.clickOnEntityName_OtherEntitiesPage(driver);   //that sub-entity or not.
          String entity_name=Helper.clickOnSubEntityName_OtherEntitiesPage(driver);
-         Helper.click_on_search_through_OtherEntities(driver);
-         Helper.go_to_advanced_Search_through_OtherEntities(driver);
+         Helper.click_on_search(driver);
+         Helper.go_to_advanced_Search(driver);
          Helper.enter_data_in_Entity_Textfield_InAdvancedSearchPage(driver,entity_name);
          Helper.clickOnSearch_InAdvancedSearchPage(driver);
          Helper.waitFor();
@@ -83,15 +79,15 @@ public class TestSuite_Other_Entities
          assertTrue(body.contains(entity_name));
      }
      @Test
-     public void testEntityNumberAdvancedSearch()
-   {
-       int number=0,actual_number=0;
+     public void testEntityNumberAdvancedSearch() //this test case clicks on a sub-entity type,stores number in front of itit,(eg 69 in front of United States),navigates till advanced search page
+   {                                            //enters that sub-entity type in "Entity" textfield,clicls on search, and then checks whether the number messages opened
+       int number=0,actual_number=0;          //is equals to the number stored or not.
        try {
            Helper.clickOnEntityName_OtherEntitiesPage(driver);
            number = Helper.message_number_OtherEntitiesPage(driver);
            String entity_name = Helper.clickOnSubEntityName_OtherEntitiesPage(driver);
-           Helper.click_on_search_through_OtherEntities(driver);
-           Helper.go_to_advanced_Search_through_OtherEntities(driver);
+           Helper.click_on_search(driver);
+           Helper.go_to_advanced_Search(driver);
            Helper.enter_data_in_Entity_Textfield_InAdvancedSearchPage(driver, entity_name);
            Helper.clickOnSearch_InAdvancedSearchPage(driver);
            actual_number = Helper.number_of_messages_opened_after_clicking_on_subEntity_name(driver);
@@ -105,8 +101,8 @@ public class TestSuite_Other_Entities
        }
    }
     @Test
-    public void testNumberOtherEntities()
-    {
+    public void testNumberOtherEntities() //this test case stores the number displayed in front of Entity name(eg 460 in front of "Place"),then clicks on
+    {                                    //that particular entity  and checks whether the number is equal to the number of contacts on that page or not.
         int number=0,number1=0;
         try {
             number = Helper.number_of_entities_OtherEntites(driver);
@@ -123,12 +119,12 @@ public class TestSuite_Other_Entities
         }
     }
     @Test
-    public void testSent_and_Received()
-    {
+    public void testSent_and_Received() //this test case checks whether the sum of sent and received messages present in message window of a particular
+    {                                       //Other-entity is greater than or equal to the number of messages opened or not..
         Helper.clickOnEntityName_OtherEntitiesPage(driver);
         Helper.clickOnSubEntityName_OtherEntitiesPage(driver);
         Helper.waitFor();
-        driver.get("http://localhost:9099/epadd/browse?archiveID=84e8afd01303201b1bb2e763c78c2df0f7133d3004e1ce6e04d9b5e2e3d15423&adv-search=1&term=%22United%20States%22&termBody=on&termSubject=on");
+       // driver.get("http://localhost:9099/epadd/browse?archiveID=84e8afd01303201b1bb2e763c78c2df0f7133d3004e1ce6e04d9b5e2e3d15423&adv-search=1&term=%22United%20States%22&termBody=on&termSubject=on");
         int number_of_sentmessages=Helper.sent_messages_in_OtherEntitiesPage(driver);
         int number_of_receivedmessages=Helper.received_messages_in_OtherEntitiesPage(driver);
         int sum=number_of_sentmessages+number_of_receivedmessages;
